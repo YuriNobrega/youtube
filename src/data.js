8b26670b84c5b3e5083221237d102fdcc2316591
YuriNobrega <yurij.nq@gmail.com>
2024-09-04 18:42:46 -0400

    E-commerce website

---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/PNk7ndF/pngwing-com-1.png",
+        title: "PROMO DE VERÃO",
+        desc: "Não fique pra trás em estilo! 30% OFF para novos clientes",
+        bg: "f5fad",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/wcM8Hb6/pngegg-3.png",
+        title: "PROMO DE INVERNO",
+        desc: "Não fique pra trás em estilo! 30% OFF para novos clientes",
+        bg: "808080",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/rF37dq0/pngegg.png",
+        title: "PROMOÇÃO",
+        desc: "Não fique pra trás em estilo! 30% OFF para novos clientes",
+        bg: "301934",
+    },
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/jb7WXrM/anime-style-scary-halloween-scene.jpg",
+        title: "ANIME",
+    },
+
+    {
+        id: 2,
+        img: "https://i.ibb.co/C2dcRzn/8972568.jpg",
+        title: "GAMES",
+    },
+
+    {
+        id: 3,
+        img: "https://i.ibb.co/SDSxbyv/person-suffering-from-technology-addiction-cybersickness.jpg",
+        title: "GEEK",
+    },
+]
+
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://i0.wp.com/loja.otakubfx.com.br/wp-content/uploads/2020/02/Camiseta-Oyasumi-PunPun-Goodnight-PunPun-3-1.jpg",
+    },
+    {
+        id: 2,
+        img: "https://i0.wp.com/loja.otakubfx.com.br/wp-content/uploads/2019/05/camiseta-serial-experiments-lain-close-the-world-11.jpg",
+    },
+    {
+        id: 3,
+        img: "https://i0.wp.com/loja.otakubfx.com.br/wp-content/uploads/2020/01/camiseta-steel-ball-run-1-2.jpg",
+    },
+    {
+        id: 4,
+        img: "https://i0.wp.com/loja.otakubfx.com.br/wp-content/uploads/2021/09/camiseta-steins-gate-psy-2.jpg",
+    },
+    {
+        id: 5,
+        img: "https://i0.wp.com/loja.otakubfx.com.br/wp-content/uploads/2020/03/camiseta-castlevania-4.jpg",
+    },
+    {
+        id: 6,
+        img: "https://i0.wp.com/loja.otakubfx.com.br/wp-content/uploads/2020/11/camiseta-touhou-project-2.jpg",
+    },
+    {
+        id: 7,
+        img: "https://i0.wp.com/loja.otakubfx.com.br/wp-content/uploads/2019/10/camiseta-loli-800-4.jpg",
+    },
+    {
+        id: 8,
+        img: "https://i0.wp.com/loja.otakubfx.com.br/wp-content/uploads/2019/04/camiseta-fujoshi-3.jpg?fit=703%2C1000&ssl=1",
+    },
+]
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css'; // se existir um arquivo de estilo
+import App from './App'; // componente principal do aplicativo
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
